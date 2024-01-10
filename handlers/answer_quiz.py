b0395b4e2dbbea6000d266d59b82048e7f2b5651
Muhammad <muhammadbekmamajonov@gmail.com>
2024-01-10 16:32:54 +0500

    qo'shimcha

--- handlers/answer_quiz.py
+++ handlers/answer_quiz.py
@@ -1,12 +1,12 @@
 
 from aiogram import types
 from loader import dp, bot
-
+from data.config import CHANNEL_ID
 
 @dp.callback_query()
 async def callback_handler(callback_data: types.CallbackQuery):
-    user = await bot.get_chat_member(chat_id="@oqiladigan_kitoblar", user_id=callback_data.from_user.id)
+    user = await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=callback_data.from_user.id)
     if user.status == "left":
-        await callback_data.answer("https://t.me/oqiladigan_kitoblar kanaliga obuna bo'ling", show_alert=True)
+        await callback_data.answer(f"{CHANNEL_ID} kanaliga obuna bo'ling", show_alert=True)
     else:
         await callback_data.answer(callback_data.data, show_alert=True)
